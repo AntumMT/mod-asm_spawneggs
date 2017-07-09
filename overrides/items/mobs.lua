a2970634916e4a94faf99d1a65b19650e934eb11
AntumDeluge <antumdeluge@gmail.com>
2017-07-09 12:13:56 -0700

    [overrides] Call 'global_exists' for 'intllib' check

--- overrides/items/mobs.lua
+++ overrides/items/mobs.lua
@@ -27,7 +27,7 @@
 
 -- Boilerplate to support localized strings if intllib mod is installed.
 local S
-if minetest.get_modpath("intllib") then
+if minetest.global_exists("intllib") then
 	dofile(minetest.get_modpath("intllib").."/intllib.lua")
 	S = intllib.Getter("mobs")
 else
