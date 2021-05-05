63efe313f26a6517f42733752e03493d78dcde3e
Jordan Irwin <antumdeluge@gmail.com>
2021-05-05 04:14:02 -0700

    Whitespace cleanup

--- eggs.lua
+++ eggs.lua
@@ -1,19 +1,19 @@
 --[[ LICENSE HEADER
-  
+
   MIT License
-  
+
   Copyright © 2017 Jordan Irwin
-  
+
   Permission is hereby granted, free of charge, to any person obtaining a copy of
   this software and associated documentation files (the "Software"), to deal in
   the Software without restriction, including without limitation the rights to
   use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
   of the Software, and to permit persons to whom the Software is furnished to do
   so, subject to the following conditions:
-  
+
     The above copyright notice and this permission notice shall be included in
     all copies or substantial portions of the Software.
-  
+
   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
   IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
   FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -21,7 +21,7 @@
   LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
   OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
   SOFTWARE.
-  
+
 --]]
 
 
@@ -39,7 +39,7 @@ antum.spawneggs.addEgg = function(name, spawn, ingredients)
 	core.register_craftitem(':spawneggs:' .. name, {
 		description = name:gsub("^%l", string.upper) .. ' Spawn Egg',
 		inventory_image = 'spawneggs_' .. name ..'.png',
-		
+
 		on_place = function(itemstack, placer, target)
 			if target.type == 'node' then
 				local pos = target.above
@@ -48,14 +48,14 @@ antum.spawneggs.addEgg = function(name, spawn, ingredients)
 				if not core.settings:get_bool('creative_mode') then
 					itemstack:take_item()
 				end
-				
+
 				return itemstack
 			end
 		end
 	})
-	
+
 	antum.spawneggs.addEggRecipe(name, spawn, ingredients)
-	
+
 	-- DEBUG
 	antum.logAction('Registered spawnegg for ' .. spawn)
 end
@@ -68,7 +68,7 @@ if core.get_modpath('spawneggs') then
 		'dirt_monster', 'dungeon_master', 'oerkki', 'rat',
 		'sand_monster', 'sheep', 'stone_monster', 'tree_monster'
 	}
-	
+
 	for I in pairs(spawneggs_default) do
 		core.clear_craft({
 			output = 'spawneggs:' .. spawneggs_default[I],
@@ -94,28 +94,28 @@ end
 
 -- mobs_redo monsters
 if core.get_modpath('mobs_monster') and core.get_modpath('default') then
-	
+
 	-- Dirt monster
 	antum.spawneggs.addEgg('dirt_monster', 'mobs_monster:dirt_monster', {'default:dirt'})
-	
+
 	-- Dungeon master
 	--addLocalEgg('dungeon_master', 'mobs_monster:dungeon_master', {''}) -- needs ingredient
-	
+
 	-- Mese monster
 	antum.spawneggs.addEgg('mese_monster', 'mobs_monster:mese_monster', {'default:mese'})
-	
+
 	-- Oerkki
-	
+
 	-- Sand monster
 	antum.spawneggs.addEgg('sand_monster', 'mobs_monster:sand_monster', {'default:sand'})
 	antum.spawneggs.addEggRecipe('sand_monster', 'mobs_monster:sand_monster', {'default:desert_sand'})
-	
+
 	-- Spider
 	--antum.spawneggs.addEgg('spider', 'mobs_monster:spider', {''}) -- need ingredient & egg
-	
+
 	-- Stone monster
 	--antum.spawneggs.addEgg('stone_monster', 'mobs_monster:stone_monster', {'default:stone'}) -- DISABLED: too graphic
-	
+
 	-- Tree monster
 	antum.spawneggs.addEgg('tree_monster', 'mobs_monster:tree_monster', {'default:sapling'})
 end
